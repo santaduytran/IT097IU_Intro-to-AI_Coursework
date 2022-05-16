737fa316b99368b83be7850933906dc992a6dcdf
Trần Nguyễn Khánh Duy <71444587+santaduytran@users.noreply.github.com>
2022-05-17 06:32:31 +0700

    Add files via upload

---
+++ Lab 8. Perceptron/iris1b.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from mlxtend.plotting import plot_decision_regions
+#from sklearn.linear_model import Perceptron
+from Perceptron import Perceptron
+#from sklearn.model_selection import train_test_split
+
+#############################################################################
+print(50 * '=')
+print('Section: Training a perceptron model on the Iris dataset')
+print(50 * '-')
+df = pd.read_csv('iris.data', header=None)
+print(df.tail())
+#############################################################################
+print(50 * '=')
+print('Plotting the Iris data')
+print(50 * '-')
+
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+# plot data
+plt.scatter(X[:50, 0], X[:50, 1],
+            color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color='blue', marker='x', label='versicolor')
+
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+#############################################################################
+print(50 * '=')
+print('Training the perceptron model')
+print(50 * '-')
+
+ppn = Perceptron(epochs=10, tau=0.1)
+
+ppn.fit(X, y)
+
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of misclassifications')
+plt.tight_layout()
+plt.show()
+#############################################################################
+print(50 * '=')
+print('A function for plotting decision regions')
+print(50 * '-')
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
