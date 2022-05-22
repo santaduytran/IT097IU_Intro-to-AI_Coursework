d31d6134a79743524f4f8893577f5250444bc5b0
Trần Nguyễn Khánh Duy <71444587+santaduytran@users.noreply.github.com>
2022-05-22 22:35:40 +0700

    Update iris1b.py

--- Lab 8. Perceptron/iris1b.py
+++ Lab 8. Perceptron/iris1b.py
@@ -1,22 +1,82 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-from mlxtend.plotting import plot_decision_regions
-#from sklearn.linear_model import Perceptron
-from Perceptron import Perceptron
-#from sklearn.model_selection import train_test_split
+class Perceptron(object):
+    """Perceptron classifier.
 
-#############################################################################
-print(50 * '=')
-print('Section: Training a perceptron model on the Iris dataset')
-print(50 * '-')
-df = pd.read_csv('iris.data', header=None)
-print(df.tail())
-#############################################################################
-print(50 * '=')
-print('Plotting the Iris data')
-print(50 * '-')
+    Parameters
+    ------------
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+    n_iter : int
+        Passes over the training dataset.
+    shuffle : bool (default: True)
+        Shuffles training data every epoch if True to prevent cycles.
+    random_state : int (default: None)
+        Set random state for shuffling and initializing the weights.
+
+    Attributes
+    -----------
+    w_ : 1d-array
+        Weights after fitting.
+    errors_ : list
+        Number of misclassifications in every epoch.
+
+    """
+    def __init__(self, eta=0.01, n_iter=10,
+                 shuffle=True, random_state=None):  # new
+        self.eta = eta
+        self.n_iter = n_iter
+        self.shuffle = shuffle  # new
+        if random_state:  # new
+            np.random.seed(random_state)  # new
+
+    def fit(self, X, y):
+        """Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples and
+            n_features is the number of features.
+        y : array-like, shape = [n_samples]
+            Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            if self.shuffle:  # new
+                X, y = self._shuffle(X, y)  # new
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+    def _shuffle(self, X, y):  # new
+        """Shuffle training data"""  # new
+        r = np.random.permutation(len(y))  # new
+        return X[r], y[r]  # new
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+##############################################################
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/rasbt/python-machine-learning-book/master/code/datasets/iris/iris.data', header=None)
+df.tail()
+##############################################################
+import matplotlib.pyplot as plt
+import numpy as np
 
 # select setosa and versicolor
 y = df.iloc[0:100, 4].values
@@ -34,26 +94,22 @@ plt.scatter(X[50:100, 0], X[50:100, 1],
 plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc='upper left')
+
 plt.tight_layout()
 plt.show()
-#############################################################################
-print(50 * '=')
-print('Training the perceptron model')
-print(50 * '-')
-
-ppn = Perceptron(epochs=10, tau=0.1)
+##############################################################
+ppn = Perceptron(eta=0.1, n_iter=10)
 
 ppn.fit(X, y)
 
 plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
 plt.xlabel('Epochs')
-plt.ylabel('Number of misclassifications')
+plt.ylabel('Number of updates')
+
 plt.tight_layout()
 plt.show()
-#############################################################################
-print(50 * '=')
-print('A function for plotting decision regions')
-print(50 * '-')
+##############################################################
+from matplotlib.colors import ListedColormap
 
 def plot_decision_regions(X, y, classifier, resolution=0.02):
 
@@ -65,7 +121,8 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     # plot the decision surface
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
     plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
@@ -76,11 +133,14 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     for idx, cl in enumerate(np.unique(y)):
         plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
                     alpha=0.8, c=cmap(idx),
-                    marker=markers[idx], label=cl)
-
+                    edgecolor='black',
+                    marker=markers[idx],
+                    label=cl)
+##############################################################
 plot_decision_regions(X, y, classifier=ppn)
 plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc='upper left')
+
 plt.tight_layout()
 plt.show()
